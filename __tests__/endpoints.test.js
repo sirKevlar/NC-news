add3676de0e3bca96505199cabc4c21e7088d94d
Kev Morel <kevmorel-nc@Kevs-MBP.connect>
2022-02-17 20:50:29 +0000

    adds commentCount test

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -90,6 +90,7 @@ describe('Endpoints', () => {
             body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
             created_at: '2020-10-16T05:03:00.000Z',
             votes: 0,
+            comment_count: '0',
           });
         });
     });
@@ -110,6 +111,7 @@ describe('Endpoints', () => {
         });
     });
   });
+
   describe('PATCH /api/articles/:article_id', () => {
     test('status 201: article created', () => {
       return request(app)
