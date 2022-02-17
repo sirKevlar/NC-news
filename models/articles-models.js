725c04caa88be9c5ad46a239b0333ebacfcf2f0e
Kev Morel <kevmorel-nc@Kevs-MBP.connect>
2022-02-17 20:50:54 +0000

    commentCount green

--- models/articles-models.js
+++ models/articles-models.js
@@ -4,14 +4,17 @@ exports.selectArticles = () => {
   return db.query(`SELECT * FROM articles;`).then(({ rows }) => {
     return rows;
   });
-}
+};
 
 exports.selectArticleById = (article_id) => {
   return db
     .query(
       `
-        SELECT * FROM articles 
-        WHERE article_id = $1;
+      SELECT articles.*, COUNT(comments.article_id) AS comment_count
+      FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;
     `,
       [article_id]
     )
